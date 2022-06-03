3a24c8989f4bdd09ddd5d38db6db7c91c3c44353
Jakub Reroń <kubareron@gmail.com>
2022-06-03 15:17:43 +0200

    refactor: improvements to the container component

--- src/components/app-container.vue
+++ src/components/app-container.vue
@@ -1,25 +1,52 @@
+<script setup lang="ts">
+interface Props {
+  tag?: string
+  type?: 'narrow' | 'standard' | 'wide'
+}
+
+withDefaults(defineProps<Props>(), {
+  tag: 'div',
+})
+</script>
+
 <template>
-  <div class="container">
+  <component :is="tag" class="container" :data-type="type">
     <slot />
-  </div>
+  </component>
 </template>
 
 <style lang="scss">
 @use 'sass:map';
+@use 'sass:math';
 
 @use '~/styles/abstracts/functions' as f;
 @use '~/styles/abstracts/mixins' as m;
 @use '~/styles/abstracts/variables' as v;
 
-$px: f.create-unit-size(16);
+$base-padding-x: f.create-unit-size(16);
+$base-max-width: f.create-unit-size(640);
 
 .container {
-  --px: #{$px};
-  padding: 0 var(--px);
+  --padding-x: #{$base-padding-x};
+  --max-width: 100%;
+
+  width: min(100% - var(--padding-x), var(--max-width));
   margin: 0 auto;
 
+  &[data-type='narrow'] {
+    --max-width: #{$base-max-width};
+  }
+
+  &[data-type='standard'] {
+    --max-width: #{$base-max-width * 1.5};
+  }
+
+  &[data-type='wide'] {
+    --max-width: #{$base-max-width * 2};
+  }
+
   @include m.media(map.get(v.$breakpoints, 'md')) {
-    --px: #{$px * 2};
+    --padding-x: #{$base-padding-x * 2};
   }
 }
 </style>
